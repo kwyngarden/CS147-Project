01e7872c9493e31c49d4a3ff9a5b942641d45559
vagrant <vagrant@precise32.(none)>
2014-02-07 07:08:06 +0000

    Removed jumbotron partial, used loop to create tiles on front page

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@
  * GET home page.
  */
 
+var data = require('../data.json')
+
 exports.view = function(req, res){
   res.render('index', {
-    // params
+    'halls': data['halls'],
+    'menu': data['menuItems']
   });
 };
