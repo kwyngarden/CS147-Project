fa00a1bae2a7fad571f52fea4af012c7d73af40f
Daryl Chang <darylc123@gmail.com>
2014-02-13 21:09:38 -0800

    Added picture tiles

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var data = require('../data.json')
 exports.view = function(req, res){
   res.render('index', {
     'halls': data['halls'],
-    'menu': data['menuItems']
+    'menuItems': data['menuItems']
   });
 };
 
