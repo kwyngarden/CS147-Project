3c8eaeef5526234473543127eed1ea568ccc55a8
Daryl Chang <darylc123@gmail.com>
2014-02-13 20:40:00 -0800

    Added images and links to food pages

--- routes/dining.js
+++ routes/dining.js
@@ -3,17 +3,29 @@
  * GET dining hall page.
  */
 
+var data = require('../data.json')
+
 exports.view = function(req, res){
   var hallName = req.params.name;
+  var menu;
+  var hours;
+  for (var i=0; i<data["menuItems"].length; i++) {
+  	if (data["menuItems"][i]["dining_hall"] == hallName) {
+  		menu = data["menuItems"][i]["menu"];
+  		break;
+  	}
+  }
+
+  for (var i=0; i<data["halls"].length; i++) { 
+  	if (data["halls"][i]["name"] == hallName) {
+  		hours = data["halls"][i]["hours"];
+  	}
+  }
+  // for (var i = 0; i < data[])
   res.render('dining', {
   	'hallName': hallName,
-  	'menuItems': [
-  		{'itemName': 'Hi'},
-  		{'itemName': 'Hi'},
-  		{'itemName': 'Hi'},
-  		{'itemName': 'Hi'},
-  		{'itemName': 'Hi'},
-  		{'itemName': 'Hi'},
-  	]
+  	'menu': menu,
+  	'weekdayHours': hours['weekdays'],
+  	'weekendHours': hours['weekends']
   });
 };
