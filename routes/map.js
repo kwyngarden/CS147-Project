1f467fa4f4d5d49a57067763dd3c71f9106c3f40
vagrant <vagrant@precise32.(none)>
2014-02-28 03:02:52 +0000

    Fixed map issue

--- routes/map.js
+++ routes/map.js
@@ -3,14 +3,20 @@
  * GET map page.
  */
 
-var data = require('../data.json')
+var data = require('../data.json');
+var models = require('../models');
 
 exports.view = function(req, res){
   var lastPage = req.session.lastPage;
   req.session.lastPage = '/map';
-  res.render('map', {
-    'lastPage': lastPage,
-    'halls': data['halls'],
-    'username': req.session.username
-  });
+
+  models.Hall
+  		.find()
+  		.exec(function(err, halls){
+  			 res.render('map', {
+			    'lastPage': lastPage,
+			    'halls': halls,
+			    'username': req.session.username
+			 });
+  		});
 };
