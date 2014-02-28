d59ac5ad287fd71461c9acd27415b4ed991b4166
vagrant <vagrant@precise32.(none)>
2014-02-28 05:12:48 +0000

    Added alt page

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,116 @@ var fs = require('fs');
 // 2. Modify data JSON object (data.halls[0].property = newvalue, etc)
 // 3. fs.writeFile('../data.json', JSON.stringify(data));
 
+exports.viewAlt = function(req, res){
+    var lastPage = req.session.lastPage;
+    var username = req.session.username;
+    var favoritesArr = []
+    req.session.lastPage = '/';
+
+    models.Hall
+            .find()
+            .populate('menu')
+            .exec(callbackZero);
+
+    function callbackZero(err, halls) {
+      if(err) console.log(err);
+      for(var i = 0; i < halls.length; i++) {
+          var menu = halls[i].menu;
+          halls[i].menu = menu;
+      }
+
+      var restHalls = halls.slice(2);
+      halls = halls.slice(0, 2);
+      
+      if (!username){
+        console.log("NO USERNAME");
+        res.render('indexAlt', {
+                       'lastPage': lastPage,
+                       'halls': halls,
+                       'restHalls': restHalls,
+                       'username': req.session.username,
+                       'isSearch': false,
+                       'favorites': favoritesArr
+                    });
+      } else {
+        models.User.findOne({'username': username}, callbackOne);
+      }
+
+      function callbackOne(err, user) {
+        console.log('callbackOne');
+        var favorites = user.favorites;
+        var numLeft = favorites.length;
+        if (favorites.length == 0) {
+          console.log("NO FAVORITES");
+          res.render('indexAlt', {
+                       'lastPage': lastPage,
+                       'halls': halls,
+                       'restHalls': restHalls,
+                       'username': req.session.username,
+                       'isSearch': false,
+                       'favorites': favoritesArr
+                    });
+        }
+
+        for (var i=0; i<favorites.length; i++) {
+            models.MenuItem.findOne({'_id': favorites[i].toString()}, callbackTwo);
+        }
+
+        function callbackTwo(err, menuItem) {
+            console.log('callbackTwo');
+            models.MenuItem.find()
+                           .exec(callbackThree);
+            
+            function callbackThree(err, items) {
+                console.log('callbackThree');
+                var dining_halls = [];
+                for (var i=0; i<items.length; i++) {
+                    if (items[i].name == menuItem.name) {
+                        var hall = items[i].dining_hall;
+                        if (dining_halls.indexOf(hall) < 0) {
+                            dining_halls.push(hall);
+                        }
+                    }
+                }
+                favoritesArr.push({
+                    'name': menuItem.name,
+                    'upvotes': menuItem.upvotes,
+                    'downvotes': menuItem.downvotes,
+                    'dining_halls': dining_halls
+                });
+                numLeft--;
+
+                if (numLeft == 0) {
+                    // Populate dining halls list
+                    models.Hall
+                          .find()
+                          .populate('menu')
+                          .exec(callbackFour);
+                }
+
+                function callbackFour(err, halls) {
+                    console.log("callbackFour")
+                    if(err) console.log(err);
+                    for(var i = 0; i < halls.length; i++) {
+                        var menu = halls[i].menu;
+                        halls[i].menu = menu;
+                    }
+                    halls = halls.slice(0,2);
+                    res.render('indexAlt', {
+                       'lastPage': lastPage,
+                       'halls': halls,
+                       'restHalls': restHalls,
+                       'username': req.session.username,
+                       'isSearch': false,
+                       'favorites': favoritesArr
+                    });
+                } 
+            }
+        }
+    }
+    }
+};
+
 
 exports.view = function(req, res){
     var lastPage = req.session.lastPage;
@@ -32,7 +142,6 @@ exports.view = function(req, res){
           halls[i].menu = menu;
       }
 
-      halls = halls.slice(0, 2);
       if (!username){
         console.log("NO USERNAME");
         res.render('index', {
